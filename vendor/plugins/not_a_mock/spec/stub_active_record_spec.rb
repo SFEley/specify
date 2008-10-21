0b224eccb7d5ffd907af070df3c0764a47abd64a
Stephen Eley <sfeley@gmail.com>
2008-10-20 20:32:37 -0400

    Add mirror 'vendor/plugins/not_a_mock/'

---
+++ vendor/plugins/not_a_mock/spec/stub_active_record_spec.rb
@@ -0,0 +1,29 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require 'not_a_mock'
+
+class ExampleActiveRecord < ActiveRecord::Base
+end
+
+describe "A stubbed ActiveRecord object" do
+  
+  before do
+    @example = ExampleActiveRecord.stub_instance
+  end
+  
+  it "should return a valid id" do
+    lambda { @example.id }.should_not raise_error(NoMethodError)
+    @example.id.should be_an_instance_of(Fixnum)
+  end
+  
+  it "should return the id as a string for to_param" do
+    lambda { @example.to_param }.should_not raise_error(NoMethodError)
+    @example.to_param.should be_an_instance_of(String)
+    @example.to_param.should == @example.id.to_s
+  end
+  
+  after do
+    NotAMock::CallRecorder.instance.reset
+    NotAMock::Stubber.instance.reset
+  end
+  
+end
